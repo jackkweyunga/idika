27e1d956110aeb13ae61c97c79116c495a9caecd
jackkweyunga <jackkweyunga@gmail.com>
2024-06-22 08:36:37 +0000

    stable-1

--- examples/cuid2.py
+++ examples/cuid2.py
@@ -1,14 +1,15 @@
 from idika import with_cuid, n_with_cuid
 import time
 
+N = 15
+C = 1000000
 # generate a snowflake unique id
-print("ID: ", with_cuid())
+print("ID: ", with_cuid(N))
 
 # generate many snowflake unique ids
 start = time.time()
-ids = n_with_cuid(1000000)
+ids = n_with_cuid(C, N)
 end = time.time()
 
 print("IDs: ", len(ids))
 print("Took: ", end - start, "s")
-
